0b56b8b9ee49ec6fe555d9f2d959805fc231afbf
Goldwin Japar <golzwinsjapar@gmail.com>
2017-12-16 16:54:33 +0700

    TODO: Count all post, replies

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,6 +26,7 @@ class HomeController extends Controller
     {
         $types = Type::all();
 
+        //TODO: Kalkulasi total dari semua post
         return view('home', compact('types'));
     }
 }

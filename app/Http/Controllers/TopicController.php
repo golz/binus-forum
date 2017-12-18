e71ceb86e7dd13b794e2b980d2a15d7775130acc
Goldwin Japar <golzwinsjapar@gmail.com>
2017-12-18 18:58:31 +0700

    Sorting and filtering threads

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,14 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Thread;
 use App\Topic;
 use Illuminate\Http\Request;
 
 class TopicController extends Controller
 {
-    public function index($id){
-        $topic=Topic::find($id);
+    public function index(Request $request, $id){
+        $topic = Topic::find($id);
+        $announcements = $topic->threads->where('is_announcement', true);
+        //$threads = $topic->threads->where('is_announcement', false);
+        $threads = Thread::where('topic_id', $id)->where('is_announcement', false);
 
-        return view('topic', compact('topic'));
+
+        if(isset($request['sk']) && isset($request['sd'])){ //Sort Value
+            $sortByValue = $request['sk'];
+            $orderByValue = $request['sd'];
+
+            if($sortByValue == 't')
+                $sortBy = 'updated_at';
+            else if($sortByValue == 's')
+                $sortBy = 'title';
+            else if($sortByValue == 'v')
+                $sortBy = 'view';
+
+            if($orderByValue == 'a')
+                $orderBy = 'asc';
+            else if($orderByValue == 'd')
+                $orderBy = 'desc';
+
+            $threads = $threads->orderBy($sortBy, $orderBy);
+        }
+        $threads = $threads->paginate(5);
+
+        return view('topic', compact('topic', 'announcements', 'threads'));
     }
 }
--- resources/views/topic.blade.php
+++ resources/views/topic.blade.php
@@ -23,9 +23,7 @@
         <div class="chunk">
 
             <div id="subhead-title">
-
                 <h2 class="forum-title">{{$topic->title}}</h2>
-
             </div>
 
             <div class="search-box">
@@ -41,70 +39,81 @@
             <div id="wrap-body">
 
                 <div class="chunk-main">
-
-
-
                     <div class="action-bar top">
-
-                        <div class="buttons">
-
-                            <a href="./posting.php?style=5&amp;mode=post&amp;f=2" class="button font-icon" title="Post a new topic">
-                                <i class="fa fa-pencil"></i>New Topic					</a>
-
-                        </div>
+                        @if(Auth::check())
+                            @if(Auth::user()->role->name == 'Administrator' || $topic->topicModerators->find(Auth::user()->id) != null)
+                            <div class="buttons">
+                                <a href="./posting.php?style=5&amp;mode=post&amp;f=2" class="button font-icon" title="Post a new topic">
+                                    <i class="fa fa-pencil"></i>New Topic					</a>
+                            </div>
+                            @endif
+                        @endif
 
                         <div class="pagination">
-                            1 topic
-                            &bull; Page <strong>1</strong> of <strong>1</strong>
+                            {{$topic->threads->count()}} threads
+                            &bull; Page <strong> {{$threads->currentPage()}} </strong> of <strong> {{$threads->total()}}</strong>
+                            <br>
+                            {!! $threads->appends(Request::except('page'))->render() !!}
                         </div>
 
                     </div>
 
-
-
-
                     <div class="forumbg announcement">
                         <div class="inner">
-                            <ul class="topiclist">
-                                <li class="header">
-                                    <dl class="icon">
-                                        <dt><div class="list-inner">Announcements</div></dt>
-                                    </dl>
-                                </li>
-                            </ul>
+
+                            @if($announcements->count() > 0)
+                                <ul class="topiclist">
+                                    <li class="header">
+                                        <dl class="icon">
+                                            <dt><div class="list-inner">Announcements</div></dt>
+                                        </dl>
+                                    </li>
+                                </ul>
+                            @endif
+
                             <ul class="topiclist topics">
+                            @foreach($announcements as $announcement)
+                                    <li class="row bg1 global-announce">
+                                        <dl class="icon global_read">
+                                            <dt title="No unread posts">
 
+                                                <span class="ico_global_read"></span>
 
-                                <li class="row bg1 global-announce">
-                                    <dl class="icon global_read">
-                                        <dt title="No unread posts">
-
-                                            <span class="ico_global_read"></span>
-
-                                        <div class="list-inner">
-                                            <a href="" class="topictitle">Welcome</a><br />
-                                            <span class="topic-ap"><i class="fa fa-floppy-o"></i></span>
-                                            by
-                                            <a href="" style="color: #AA0000;" class="username-coloured">Gramziu</a> &raquo; 24 Feb 2015, 21:50
-                                            <div class="r-lastpost-container">
-                                                <a href="" title="Go to last post" class="r-lastpost"><i class="fa fa-angle-right"></i></a>
+                                            <div class="list-inner">
+                                                <a href="{{url('topic/'.$topic->id.'/thread/'.$announcement->id)}}" class="topictitle">{{$announcement->title}}</a><br />
+                                                <span class="topic-ap"><i class="fa fa-floppy-o"></i></span>
+                                                by
+                                                <a href="" style="color: @if($announcement->user->role->name == 'Administrator') #AA0000 @elseif($topic->topicModerators->find($announcement->user->id) != null) #00AA00 @endif ;" class="username-coloured">{{$announcement->user->nickname}}</a> &raquo;
+                                                {{$announcement->updated_at->format('d M Y, H:i')}}
+                                                <div class="r-lastpost-container">
+                                                    <a href="" title="Go to last post" class="r-lastpost"><i class="fa fa-angle-right"></i></a>
+                                                </div>
                                             </div>
-                                        </div>
-                                        </dt>
-                                        <dd class="posts">0 <dfn>Replies</dfn></dd>
-                                        <dd class="views">46487 <dfn>Views</dfn></dd>
-                                        <dd class="lastpost"><span><dfn>Last post </dfn>
-                                            by
-                                            <a href="" style="color: #AA0000;" class="username-coloured">Gramziu</a>
-                                            <span class="lastpostavatar">
-                                                <img class="avatar" src="" width="30" height="30" alt="User avatar" />
-                                            </span>
-                                            <a href="" title="Go to last post" class="lastpost-last"><i class="fa fa-arrow-right"></i></a>
-                                            <br />24 Feb 2015, 21:50</span>
-                                        </dd>
-                                    </dl>
-                                </li>
+                                            </dt>
+
+                                            <dd class="posts">{{$announcement->replies->count()}} <dfn>Replies</dfn></dd>
+                                            <dd class="views">{{$announcement->view}}<dfn>Views</dfn></dd>
+
+                                            @if($announcement->replies->count() > 0)
+                                            <dd class="lastpost">
+                                                <span>
+                                                    <dfn>Last post </dfn>
+                                                    Last post by
+                                                    <a href="" style="color: @if($announcement->replies->last()->user->role->name == 'Administrator') #AA0000 @elseif($topic->topicModerators->find($announcement->user->id) != null) #00AA00 @endif ;" class="username-coloured">{{$announcement->replies->last()->user->nickname}}</a>
+                                                    <span class="lastpostavatar">
+                                                        <img class="avatar" src="{{ asset('uploads/profile/'.$announcement->replies->last()->user->image) }}" width="30" height="30" alt="User avatar" />
+                                                    </span>
+                                                    <a href="" title="Go to last post" class="lastpost-last"><i class="fa fa-arrow-right"></i></a>
+                                                    <br />
+                                                    {{$announcement->replies->last()->updated_at->format('d M Y, H:i')}}
+                                                </span>
+                                            </dd>
+                                            @endif
+                                        </dl>
+                                    </li>
+                            @endforeach
                             </ul>
+
                         </div>
                     </div>
 
@@ -117,57 +126,69 @@
                                     </dl>
                                 </li>
                             </ul>
-                            <ul class="topiclist topics">
 
-                                <li class="row bg2">
-                                    <dl class="icon topic_read">
+                            <ul class="topiclist topics">
+                            @foreach($threads as $thread)
+                                <li class="row bg1 global-announce">
+                                    <dl class="icon global_read">
                                         <dt title="No unread posts">
 
-                                            <span class="ico_topic_read"></span>
+                                            <span class="ico_global_read"></span>
 
                                         <div class="list-inner">
-                                            <a href="{{ url('topic/'.$topic->id.'/thread/1999') }}" class="topictitle">TESTING YANG INI AJA</a><br />
-
-                                            by <a href="" style="color: #AA0000;" class="username-coloured">Goldwin</a> &raquo; 24 Feb 2015, 20:20
+                                            <a href="{{url('topic/'.$topic->id.'/thread/'.$thread->id)}}" class="topictitle">{{$thread->title}}</a><br />
+                                            <span class="topic-ap"><i class="fa fa-floppy-o"></i></span>
+                                            by
+                                            <a href="" style="color: @if($thread->user->role->name == 'Administrator') #AA0000 @elseif($topic->topicModerators->find($thread->user->id) != null) #00AA00 @endif ;" class="username-coloured">{{$thread->user->nickname}}</a> &raquo;
+                                            {{$thread->updated_at->format('d M Y, H:i')}}
                                             <div class="r-lastpost-container">
                                                 <a href="" title="Go to last post" class="r-lastpost"><i class="fa fa-angle-right"></i></a>
                                             </div>
                                         </div>
                                         </dt>
-                                        <dd class="posts">0 <dfn>Replies</dfn></dd>
-                                        <dd class="views">20951 <dfn>Views</dfn></dd>
-                                        <dd class="lastpost"><span><dfn>Last post </dfn>
-                                            by
-                                            <a href="" style="color: #AA0000;" class="username-coloured">Gramziu</a>
+
+                                        <dd class="posts">{{$thread->replies->count()}} <dfn>Replies</dfn></dd>
+                                        <dd class="views">{{$thread->view}}<dfn>Views</dfn></dd>
+
+                                        @if($thread->replies->count() > 0)
+                                            <dd class="lastpost">
+                                        <span>
+                                            <dfn>Last post </dfn>
+                                            Last post by
+                                            <a href="" style="color: @if($thread->replies->last()->user->role->name == 'Administrator') #AA0000 @elseif($topic->topicModerators->find($thread->user->id) != null) #00AA00 @endif ;" class="username-coloured">{{$thread->replies->last()->user->nickname}}</a>
                                             <span class="lastpostavatar">
-                                                <img class="avatar" src="" width="30" height="30" alt="User avatar" />
+                                                <img class="avatar" src="{{ asset('uploads/profile/'.$thread->replies->last()->user->image) }}" width="30" height="30" alt="User avatar" />
                                             </span>
-                                            <a href="" title="Go to last post" class="lastpost-last">
-                                                <i class="fa fa-arrow-right"></i>
-                                            </a> <br />24 Feb 2015, 20:20</span>
-                                        </dd>
+                                            <a href="" title="Go to last post" class="lastpost-last"><i class="fa fa-arrow-right"></i></a>
+                                            <br />
+                                            {{$thread->replies->last()->updated_at->format('d M Y, H:i')}}
+                                        </span>
+                                            </dd>
+                                        @endif
                                     </dl>
                                 </li>
-
+                            @endforeach
                             </ul>
                         </div>
                     </div>
 
-                    <form method="post" action="">
+                    <form method="get" action="">
                         {{ csrf_field() }}
                         <fieldset class="display-options">
+                            <input type="hidden" name="page" id="page" value="{{$threads->currentPage()}}">
                             <label>Sort by
                                 <select name="sk" id="sk">
-                                    <option value="a">Author</option>
+                                    {{--<option value="a">Author</option>--}}
                                     <option value="t" selected="selected">Post time</option>
-                                    <option value="r">Replies</option><option value="s">Subject</option>
+                                    {{--<option value="r">Replies</option>--}}
+                                    <option value="s">Subject</option>
                                     <option value="v">Views</option>
                                 </select>
                             </label>
                             <label>
                                 <select name="sd" id="sd">
-                                    <option value="a">Ascending</option>
-                                    <option value="d" selected="selected">Descending</option>
+                                    <option value="a" selected="selected">Ascending</option>
+                                    <option value="d">Descending</option>
                                 </select>
                             </label>
                             <input type="submit" name="sort" value="Go" class="button2" />
@@ -175,19 +196,22 @@
                     </form>
 
                     <div class="action-bar bottom">
-                        <div class="buttons">
-
-                            <a href="" class="button font-icon" title="Post a new topic">
-                                <i class="fa fa-pencil"></i>
-                                New Topic
-                            </a>
-
-                        </div>
+                        @if(Auth::check())
+                            @if(Auth::user()->role->name == 'Administrator' || $topic->topicModerators->find(Auth::user()->id) != null)
+                                <div class="buttons">
+                                    <a href="./posting.php?style=5&amp;mode=post&amp;f=2" class="button font-icon" title="Post a new topic">
+                                        <i class="fa fa-pencil"></i>New Topic					</a>
+                                </div>
+                            @endif
+                        @endif
 
                         <div class="pagination">
-                            1 topic
-                            &bull; Page <strong>1</strong> of <strong>1</strong>
+                            {{$topic->threads->count()}} threads
+                            &bull; Page <strong> {{$threads->currentPage()}} </strong> of <strong> {{$threads->total()}}</strong>
+                            <br>
+                            {!! $threads->appends(Request::except('page'))->render() !!}
                         </div>
+
                     </div>
 
 
